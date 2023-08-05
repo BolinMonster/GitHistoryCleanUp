cd2078255b23623625fb7cea5e0ca0960e43a6c9
Houston <81814923+r4ch1d76@users.noreply.github.com>
2023-08-05 13:53:59 +0200

    Update cleanup_git_history.bat

--- cleanup_git_history.bat
+++ cleanup_git_history.bat
@@ -2,6 +2,8 @@
 
 REM Nettoie l'historique des commits Git en transformant le commit actuel en le commit initial
 
+set BRANCH_NAME=main
+
 REM Checkout vers une branche temporaire
 git checkout --orphan latest_branch
 
@@ -11,14 +13,14 @@ git add -A
 REM Fait le commit initial
 git commit -am "Commit initial"
 
-REM Supprime la branche main
-git branch -D main
+REM Supprime la branche %BRANCH_NAME%
+git branch -D %BRANCH_NAME%
 
-REM Renomme la branche temporaire en main
-git branch -m main
+REM Renomme la branche temporaire en %BRANCH_NAME%
+git branch -m %BRANCH_NAME%
 
-REM Pousse les changements vers la branche main
-git push -f origin main
+REM Pousse les changements vers la branche %BRANCH_NAME%
+git push -f origin %BRANCH_NAME%
 
 REM Supprime les anciens fichiers
 git gc --aggressive --prune=all
